a13d97e8dd51c9b4000422a9ca176146f11a2b99
dukuan <tsytiansiyu_i@didiglobal.com>
2021-03-25 17:57:34 +0800

    c

--- template.js
+++ template.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const fs = require('fs')
 const absolutePath = '/Users/didi/Desktop/pub'
+// const absolutePath = '/root/ecmc8026/pub'
 exports.getTemplate =function(){
     let paths = fs.readdirSync(absolutePath)
     // paths.forEach(item =>{
@@ -13,8 +14,10 @@ exports.getTemplate =function(){
     paths.forEach(item => {
     template+=`<input type="checkbox" name=${item}> ${item}`
     })
-    template +='<br /><button onclick="download()">点击下载</button>'
-    template +=`<script>function download(){let checkedNodes = document.querySelectorAll('input:checked'); let names = Array.prototype.call(checkedNodes).map(item=>item.name); console.log(name)}</script>`
+    
+    template +='<br /><br /><br /><br /><br />背景介绍：复选框实例   &lt;input type="checkbox" name=ams>'
+    template +='<br />先随便选择几个文件，然后我的问题是：如何通过原生js获取勾选的文件名,如  ams，dstor'
+   
     return template
 }
 
